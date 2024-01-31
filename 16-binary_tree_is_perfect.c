c4d731216a9d9106ba3a58482db66d784bee1750
Avumile <Smithavery435@gmail.com>
2024-01-31 02:14:07 -0800

    trial

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,9 @@
 #include "binary_trees.h"
+#include <stddef.h>
+
 int is_perfect_recursive(const binary_tree_t *tree, int depth, int level);
 size_t binary_tree_height(const binary_tree_t *tree);
+
 /**
  * binary_tree_is_perfect - Checks if a binary tree is perfect
  * @tree: Pointer to the root node of the tree to check.
@@ -44,20 +47,36 @@ int is_perfect_recursive(const binary_tree_t *tree, int depth, int level)
 			is_perfect_recursive(tree->right, depth, level + 1));
 }
 
+
+/**
+ * binary_tree_height - Measures the height of a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height.
+ *
+ * Return: Height of the tree, 0 if tree is NULL
+ */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+	size_t left_height = 0, right_height = 0, tree_height;
 
 	/* Base case: if tree is NULL, return 0 */
 	if (tree == NULL)
 		return (0);
 
 	/* Recursive case: calculate the height of the left subtree */
-	left_height = binary_tree_height(tree->left);
+	if (tree->left != NULL)
+		left_height = 1 + binary_tree_height(tree->left);
 
 	/* Recursive case: calculate the height of the right subtree */
-	right_height = binary_tree_height(tree->right);
+	if (tree->right != NULL)
+		right_height = 1 + binary_tree_height(tree->right);
 
-	/* Determine the maximum height between left and right subtrees, add 1 for current level */
-	return (1 + (left_height > right_height ? left_height : right_height));
+	/* Determine the maximum height between left and right subtrees */
+	if (left_height > right_height)
+		tree_height = left_height;
+	else
+		tree_height = right_height;
+
+	/* Return the calculated height */
+	return (tree_height);
 }
+
