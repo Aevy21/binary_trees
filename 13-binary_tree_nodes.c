f6d5c619543bce50979d4b91ab199225dbc79a15
Avumile <Smithavery435@gmail.com>
2024-01-30 18:22:43 -0800

    13 problematic

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - Counts nodes with at least 1 child in a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Number of nodes with at least 1 child, 0 if tree is NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    if (tree == NULL)
+        return 0;
+
+    size_t left_nodes = binary_tree_nodes(tree->left);
+    size_t right_nodes = binary_tree_nodes(tree->right);
+
+    if (tree->left != NULL || tree->right != NULL)
+        return 1 + left_nodes + right_nodes;
+
+    return 0;
+}
+
