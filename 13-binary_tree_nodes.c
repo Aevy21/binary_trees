928dee5d52239a8e0a1f5274dc168329fb1e62f3
Avumile <Smithavery435@gmail.com>
2024-01-30 18:33:03 -0800

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,20 +9,20 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t left_nodes = 0, right_nodes = 0;
+	size_t left_nodes = 0, right_nodes = 0;
 
-    if (tree == NULL)
-        return 0;
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left != NULL)
-        left_nodes = binary_tree_nodes(tree->left);
+	if (tree->left != NULL)
+		left_nodes = binary_tree_nodes(tree->left);
 
-    if (tree->right != NULL)
-        right_nodes = binary_tree_nodes(tree->right);
+	if (tree->right != NULL)
+		right_nodes = binary_tree_nodes(tree->right);
 
-    if (tree->left != NULL || tree->right != NULL)
-        return 1 + left_nodes + right_nodes;
+	if (tree->left != NULL || tree->right != NULL)
+		return (1 + left_nodes + right_nodes);
 
-    return 0;
+	return (0);
 }
 
